fb68dd653f19594ce398f45b1cf24ef2a92c66ee
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-02-17 10:59:06 +0600

    error handeling

--- js/main.js
+++ js/main.js
@@ -1,26 +1,56 @@
-document.getElementById('btn-calculate').addEventListener('click', function(){
-    const getIncome = getInputValue('income-input');
-    const foodCost = getInputValue('food-cost');
-    const rentCost = getInputValue('rent-cost');
-    const clothCost = getInputValue('cloth-cost');
+document.getElementById("btn-calculate").addEventListener("click", function () {
+  const getIncome = getInputValue("income-input");
+  const foodCost = getInputValue("food-cost");
+  const rentCost = getInputValue("rent-cost");
+  const clothCost = getInputValue("cloth-cost");
+  if (
+    isNaN(getIncome) ||
+    isNaN(foodCost) ||
+    isNaN(rentCost) ||
+    isNaN(clothCost)
+  ) {
+    alert("Please enter only Number");
+    const inputTag = document.getElementsByTagName("input");
+    for (const tag of inputTag) {
+      tag.value = "";
+    }
+  } else {
     const totalExpense = foodCost + rentCost + clothCost;
     const balance = getIncome - totalExpense;
-    setInnerText('total-expense', totalExpense);
-    setInnerText('balance', balance);
-})
-
-document.getElementById('btn-save').addEventListener('click', function(){
-    const getIncome = getInputValue('income-input');
-    const getSavings = getInputValue('saving-input');
-    const savingNumber = getSavings / 100 ;
-    const savingAmount = getIncome * savingNumber;
-    setInnerText('saving-amount', savingAmount);
-    const balanceValue = getInnerTextNumber('balance');
-    const remainingBalance = balanceValue - savingAmount;
-    setInnerText('remaining-balance', remainingBalance);
-    // clear input data
-    const inputTag = document.getElementsByTagName('input');
-    for(const tag of inputTag){
-        tag.value = '';
+    if (totalExpense > getIncome) {
+      alert("You have no enough income for expense!");
+      const inputTag = document.getElementsByTagName("input");
+      for (const tag of inputTag) {
+        tag.value = "";
+      }
+    } else {
+      setInnerText("total-expense", totalExpense);
+      setInnerText("balance", balance);
     }
-})
+  }
+});
+
+document.getElementById("btn-save").addEventListener("click", function () {
+  const getIncome = getInputValue("income-input");
+  const getSavings = getInputValue("saving-input");
+  if (isNaN(getSavings)) {
+    alert("Enter only number!");
+    document.getElementById("saving-input").value = "";
+  } else {
+    const savingNumber = getSavings / 100;
+    const savingAmount = getIncome * savingNumber;
+    setInnerText("saving-amount", savingAmount);
+    const balanceValue = getInnerTextNumber("balance");
+    const remainingBalance = balanceValue - savingAmount;
+    if (savingAmount > balanceValue) {
+      alert("You have no enough balance for saving!");
+    } else {
+      setInnerText("remaining-balance", remainingBalance);
+      // clear input data
+      const inputTag = document.getElementsByTagName("input");
+      for (const tag of inputTag) {
+        tag.value = "";
+      }
+    }
+  }
+});
--- js/utilities.js
+++ js/utilities.js
@@ -1,17 +1,17 @@
 // get input value in Number
-function getInputValue(inputId){
+function getInputValue(inputId) {
     const input = document.getElementById(inputId);
     const inputNumber = parseInt(input.value);
     return inputNumber;
 }
 // Get innerText in Number
-function getInnerTextNumber(inputId){
-    const input = document.getElementById(inputId);
-    const innerText = parseFloat(input.innerText);
-    return innerText;
+function getInnerTextNumber(inputId) {
+  const input = document.getElementById(inputId);
+  const innerText = parseFloat(input.innerText);
+  return innerText;
 }
 //  set innerText
-function setInnerText(setId, newValue){
-    const findText = document.getElementById(setId);
-    findText.innerText = newValue;
+function setInnerText(setId, newValue) {
+  const findText = document.getElementById(setId);
+  findText.innerText = newValue;
 }
